5c9183cb4913321f56c97f90a24ce3d732a442ae
Sean Johnson <sjohnson@sjohnsonaz.com>
2018-10-08 11:29:57 -0700

    Changed test to use views.

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -3,8 +3,9 @@ import chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 let expect = chai.expect;
 
-import Sierra, { Controller, middleware, route, Context } from 'sierra';
+import Sierra, { Controller, middleware, route, Context, view } from 'sierra';
 import { wait } from '../scripts/utils/TestUtil';
+import SierraHandlebars from '../scripts/SierraHandlebars';
 
 describe('route decorator`', () => {
     let port = 3005;
@@ -14,12 +15,14 @@ describe('route decorator`', () => {
         class TestController extends Controller {
             @route('get')
             async get(context: Context, value: any) {
-                return { value: true };
+                return view({ value: true });
             }
         }
 
         application = new Sierra();
         application.addController(new TestController());
+        SierraHandlebars.viewRoot = './views/';
+        application.view(SierraHandlebars.handle);
         application.init();
         await application.listen(port);
     });
